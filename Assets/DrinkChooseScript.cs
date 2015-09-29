34a3d7ac043fdf0d6ad36d94e08f0d29f1f0c649
norbish <torsun@hotmail.com>
2015-09-29 08:54:08 +0200

    3rd

--- Assets/DrinkChooseScript.cs
+++ Assets/DrinkChooseScript.cs
@@ -7,15 +7,12 @@ public class DrinkChooseScript : MonoBehaviour {
 	private SerialPort ArduinoDue = new SerialPort("COM3");
 	private SerialPort ArduinoDueIN = new SerialPort("COM5");
 	private string DrinkString;
-	private string MixString;
 	public string comPort = "COM3";
 	public string comPortIn = "COM5";
-	private byte[] barray = {0,0,0,0,0,0,0,0};
-	// Use this for initialization
 	void Start () 
 	{
 		
-		ArduinoDue.BaudRate = 9600;//kan hende jeg bare må ha 1 script, hvis den kjører alle 4 ganger pga. knappene.
+		ArduinoDue.BaudRate = 9600;
 		ArduinoDue.PortName = comPort;//COM connected to BT
 		ArduinoDue.Open();
 
@@ -60,7 +57,6 @@ public class DrinkChooseScript : MonoBehaviour {
 
 			while(true)
 			Debug.Log (ArduinoDueIN.ReadExisting());
-		Debug.Log ("tihihi");
 
 	}
 	public void onClick (string name)
@@ -68,46 +64,29 @@ public class DrinkChooseScript : MonoBehaviour {
 		if (name == "Drink1") 
 		{
 			DrinkString = "1,0,0,1";
-			MixString = "0000";
-
-			byte[] DrinkByte={1,0,0,1,0,0,0,0};
-			//ArduinoDue.Write(DrinkByte, 0, DrinkByte.Length);
 			
 		}
 		if (name == "Drink2") 
 		{
 			DrinkString = "0,1,0,1";
-			MixString = "0000";
-
-			byte[] DrinkByte={0,1,0,1,0,0,0,0};
-			//ArduinoDue.Write(DrinkByte, 0, DrinkByte.Length);
 			
 		}if (name == "Drink3") 
 		{
 			DrinkString = "0,0,1,1";
-			MixString = "0000";
-
-			byte[] DrinkByte={0,0,1,1,0,0,0,0};
-			//ArduinoDue.Write(DrinkByte, 0, DrinkByte.Length);
 			
 		}if (name == "Drink4") 
 		{
 			DrinkString = "1,0,1,1";
-			MixString = "0000";
-
-			byte[] DrinkByte={1,0,1,1,0,0,0,0};
-			//ArduinoDue.Write(DrinkByte, 0, DrinkByte.Length);
 			
 		}
 		
 		
 		
 		Debug.Log (DrinkString);
-		Debug.Log (MixString);
 		ArduinoDue.WriteLine (DrinkString);
 		//ArduinoDue.WriteLine (MixString);
 
-		Debug.Log (ArduinoDueIN.ReadExisting());
+		//Debug.Log (ArduinoDueIN.ReadExisting());
 		
 		//Thread.Sleep (500);
 
